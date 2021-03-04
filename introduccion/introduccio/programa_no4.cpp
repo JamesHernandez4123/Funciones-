d6dee979a5ead28093ea5879932ae0f29ce8e123
JamesHernandez4123 <79390692+JamesHernandez4123@users.noreply.github.com>
2021-03-04 00:10:05 -0600

    Add files via upload

---
+++ introduccion/introduccio/programa_no4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int encontrarMax(int x, int y);
+
+int main(){
+    int n1, n2, n3;
+    cout<<"introduzca el valor del primer numero:"; cin>>n1;
+    cout<<"Introduzca el valor del segundo numero:"; cin>>n2;
+    cout<<"Introduzca el valor del tercer numero:"; cin>>n3;
+    int max1= encontrarMax(encontrarMax(n1,n2),n3);
+    cout<<"El numero mayor es:"<<max1;
+
+    getch();
+    return 0;
+}
+int encontrarMax(int x, int y){
+    int c;
+    if(x>y){
+        c=x;
+    }
+    else{
+        c=y;}
+    return c;
+}
